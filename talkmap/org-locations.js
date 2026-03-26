b616066f4bce20587797b02d6168be8bea6708fa
Jacob (Yankl) Mazor <n.jacobmazor@gmail.com>
2026-03-26 05:19:23 -0400

    Update org-locations.js

--- talkmap/org-locations.js
+++ talkmap/org-locations.js
@@ -1,22 +1,9 @@
 var addressPoints = [
-  [
-    "Berkeley CA, USA",
-    37.8708393,
-    -122.2728638
-  ],
-  [
-    "London, UK",
-    51.5073219,
-    -0.1276473
-  ],
-  [
-    "San Francisco, California",
-    37.7792808,
-    -122.4192362
-  ],
-  [
-    "Los Angeles, CA",
-    34.0543942,
-    -118.2439408
-  ]
+  ["Combining rational maps and Kleinian reflection groups - UIC, Oct 2023", 41.8719, -87.6484],
+  ["Geodesic flows for iterated rational maps - TIFR, Sep 2024", 18.9, 72.8],
+  ["Maps from trees - KSoM, Dec 2024", 11.2588, 75.7804],
+  ["Mating Kleinian groups - Boston University, Apr 2025", 42.3505, -71.1054],
+  ["Mating Kleinian groups - CUNY, Apr 2025", 40.7486, -73.9838],
+  ["Mating Kleinian groups - Dartmouth, Apr 2025", 43.7044, -72.2887],
+  ["When zippers virtually fiber - Stony Brook, May 2025", 40.9132, -73.1234]
 ];
